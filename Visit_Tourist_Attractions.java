b0afcb73c7e019473fb63846420306be72323b9e
Harshitha Gowda <harshithagm0207@gmail.com>
2016-07-05 22:50:54 -0500

    visit functionality working

--- Visit_Tourist_Attractions.java
+++ Visit_Tourist_Attractions.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import org.json.JSONArray;
@@ -17,12 +19,9 @@ import org.json.JSONObject;
 public class Visit_Tourist_Attractions extends AppCompatActivity {
 
     String url = null;
-    String location = null;
+    String latlng[] = null;
     Bundle extras;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +30,12 @@ public class Visit_Tourist_Attractions extends AppCompatActivity {
 
         extras = getIntent().getExtras();
         if (extras != null) {
-            location = extras.getString("location");
-            location = location.replaceAll(" ", "%20");
+            latlng = extras.getStringArray("latlng");
+            //System.out.println("The cordinates fetched are:..........."+latlng[0]+latlng[1]);
 
         }
         Call call = new Call();
-        call.execute(location);
+        call.execute(latlng);
 
 
     }
@@ -47,24 +46,61 @@ public class Visit_Tourist_Attractions extends AppCompatActivity {
         JSONArray jsonArr;
         String[] lm;
         String hotelName;
+        String Name=null;
+
+
 
         @Override
         protected JSONArray doInBackground(String... params) {
-            url = "http://terminal2.expedia.com/x/geo/features?within=1km&lng=-122.453269&lat=37.777363&type=point_of_interest&apikey=L2ZB8VtZrd2AKA8ZjvysemHTVJAWIrMC";
-            //http://terminal2.expedia.com/x/geo/features?ln.op=cn&ln.value=seattle%20center&limit=5&apikey=L2ZB8VtZrd2AKA8ZjvysemHTVJAWIrMC";
-            //"http://terminal2.expedia.com/x/geo/features?within=1km&lng=-122.453269&lat=37.777363&type=point_of_interest&apikey=L2ZB8VtZrd2AKA8ZjvysemHTVJAWIrMC";
+
+            url = "http://terminal2.expedia.com/x/geo/features?within=1km&lng="+latlng[1]+"&lat="+latlng[0]+"&type=point_of_interest&apikey=L2ZB8VtZrd2AKA8ZjvysemHTVJAWIrMC";
+
             jsonArr = RestParser.getResponseForUrl(url, "GET");
             System.out.println("Trying to print........." + params[0]);
             return jsonArr;
         }
 
         @Override
-        protected void onPostExecute(JSONArray result) {
+        protected void onPostExecute(final JSONArray result) {
 
             lm = JsonParser(result);
             ListView r_listView = (ListView) findViewById(R.id.listView);
             ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,lm);
             r_listView.setAdapter(adapter);
+            r_listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    System.out.println("Inside onclick");
+                    Name = lm[position];
+                    System.out.println("trying to send the name of the place"+Name);
+
+                    try{
+
+                        for(int j=0;j<result.length();j++){
+                            String coordinates;
+                            String name;
+                            JSONObject value = result.getJSONObject(j);
+                            name = value.getString("name");
+                            if(name.equals(lm[position])){
+
+                                JSONObject pos = value.getJSONObject("position");
+                                coordinates = pos.getString("coordinates");
+                                System.out.println("the tourist place coordinates are :"+coordinates);
+
+                                Intent navigate = new Intent(Visit_Tourist_Attractions.this,MapsActivity.class);
+                                navigate.putExtra("coordinates",coordinates);
+                                startActivity(navigate);
+                            }
+                            else
+                                continue;
+                        }
+                    }
+                    catch(Exception Ex){
+                        Ex.printStackTrace();
+                    }
+                }
+            });
 
 
         }
@@ -78,7 +114,7 @@ public class Visit_Tourist_Attractions extends AppCompatActivity {
                     String name;
                     JSONObject item = jsonobj.getJSONObject(i);
                     name = item.getString("name");
-                    System.out.println("print the name of the hotel....."+name);
+                    System.out.println("print the name of the places....."+name);
                     lm[i]=name;
                 }
             }
